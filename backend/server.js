ce646859d6b82664bac7c6395013257cd6219e96
Felix Wang <felix-wang@outlook.de>
2021-03-10 01:18:43 +0100

    set up socket.io

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,31 @@
-const express = require('express')
-const app = express()
-
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
+const path = require('path');
 const PORT = 3000
 
-app.get('/', (req, res) => {
-    res.send("<p>Hallo Welt</p>")
+app.use(express.static('public'))
+
+io.on("connection", (socket) => {
+
+    socket.on('we rollin', (msg) => {
+       console.log("we rollin'");
+        io.emit('ttt', "we rollin' on id: "+msg);
+    });
+
+    io.emit('otto', "Hier ist Otto")
+    console.log('a user connected');
+
+    socket.on('disconnect', () => {
+        console.log('user disconnected')
+    });
 })
 
-app.listen(PORT, () => {
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve('public/html/index.html'))
+})
+
+server.listen(PORT, () => {
     console.log('Server is running.')
 })
